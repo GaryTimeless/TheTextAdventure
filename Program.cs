a199ea7aacee1ee19fe0b0bde887162e0d46fb96
nandoruck <60364957+nandoruck@users.noreply.github.com>
2020-02-05 20:30:38 +0000

    nando:ersterversuch

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text.RegularExpressions;
+
+
+namespace Text_Based_Adventure
+{
+
+
+    public class Program
+    {
+        public static void Main()
+        {
+
+            Text.Adv.mit_Greg.Start.CreatePlayer();
+            
+            
+        
+        
+        }
+
+    }
+}
