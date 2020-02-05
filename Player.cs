f08e63a1e15492cf4d53714c370cf631622ab9a7
nandoruck <60364957+nandoruck@users.noreply.github.com>
2020-02-05 21:30:25 +0000

    Add files via upload

---
+++ Player.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Text.Adv.mit_Greg
+{
+    class Player1//
+    {
+        public string name;
+        public int coins = 0;
+        public int health = 0;
+        public int damage = 0;
+        public int armorValue = 0;
+        public int potion = 0;
+        public int weaponValue = 0;
+        public int runspeed = 0;
+        //public int id;
+
+        public int mods = 0;
+        //public City CurrentCity;
+        // public string ImportCity;
+
+        public static Player1 p = new Player1();
+
+        
+    }
+}
---
+++ Start.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text.RegularExpressions;
+
+namespace Text.Adv.mit_Greg
+{
+    public class Start
+    {
+
+        public static Player currentPlayer = new Player();
+        public static bool mainloop = true;
+       
+        public static Random rand = new Random();
+
+
+        static Player NewStart(int i)
+        {
+            Console.Clear();
+            Player p = new Player();
+
+            Console.WriteLine(" Willkomen in Text Adventure ");
+
+
+
+            Console.WriteLine("Wie heißt du Krieger?");
+
+        
+
+            currentPlayer.name = Console.ReadLine();
+            Console.ReadKey();
+            Console.Clear();
+            return p;
+        }
+        public static void CreatePlayer()
+        {
+            Player p = new Player();
+
+            Console.WriteLine(" Willkomen in Text Adventure ");
+
+
+
+            Console.WriteLine("Wie heißt du Krieger?");
+
+            currentPlayer.name = Console.ReadLine();
+        }
+    }
+}
