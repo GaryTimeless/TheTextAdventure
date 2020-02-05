154cc3250e566c5efa84a0ff9e6d3c7b8222ec38
nandoruck <60364957+nandoruck@users.noreply.github.com>
2020-02-05 21:04:02 +0000

    Hallo Greg

---
+++ Start.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text.RegularExpressions;
+
+namespace Text.Adv.mit_Greg
+{
+    public class Start
+    {
+        public static Player currentPlayer = new Player();
+       
+        public static void CreatePlayer()
+        {
+            Player p = new Player();
+
+            Console.WriteLine(" Willkomen in Text Adventure ");
+
+
+
+            Console.WriteLine("Wie heißt du Krieger?");
+
+            currentPlayer.name = Console.ReadLine();
+        }
+    }
+}
