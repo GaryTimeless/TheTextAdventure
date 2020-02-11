55141846c2b724921105211eb39c92f9b7bd2ca6
Gary <Gary.timeless@gmail.com>
2020-02-11 13:29:37 +0100

    Greg Test
    keine absicht

---
+++ EmptyCSharpFile.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public class Programm
+{
+
+    static Random rand = new Random();
+
+
+  // public static string GegnerString
+    //{
+    
+    
+
+   // return gegner[0];
+   // }
+
+    public static void Main()
+    {
+
+        string[] gegner = { "Zombie", "Ritter", "Wache" };
+        int randomInt = rand.Next(0, 2);
+
+        Console.WriteLine(gegner[randomInt]);
+
+        Console.WriteLine(gegner[randomInt]);
+        Console.WriteLine(gegner[randomInt]); Console.WriteLine(gegner[randomInt]);
+        Console.WriteLine(gegner[randomInt]);
+
+    }
+}
